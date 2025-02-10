4173b9f495b8e409ecfbb93467627a9db343d116
AN <itsaksht@gmail.com>
2025-02-10 17:31:08 +0530

    [features added] : resizing, hovering, new grid formation

--- sketch.js
+++ sketch.js
@@ -1,19 +1,63 @@
-const fragment1 = document.createDocumentFragment();
-for (let i=1; i<=16; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.id = `row-${i}`;
-    newDiv.style.display = "flex";
-    fragment1.append(newDiv);
-}
-document.body.appendChild(fragment1);
+const btn = document.getElementById("button");
+let side =16;
+let gridContainer;
 
-for (let i=1; i<=16; i++) {
-    let rowDiv = document.getElementById(`row-${i}`);
-    let fragment2 = document.createDocumentFragment();
-    for (let j=1; j<=16; j++) {
-        let innerDiv = document.createElement("div");
-        innerDiv.className = `inner-${i}-${j}`;
-        fragment2.append(innerDiv);
+function getSize() {
+    
+    side = parseInt(prompt("What is the size of the square you would like?:", "16"));
+    if (isNaN(side) || side <=1 || side >100) {
+        alert("Please a number between 1 and 100");
+        getSize();
+        return;
     }
-    rowDiv.append(fragment2);
+    removeContainer();
+    createContainer();
+    drawGrid(side)
+
+}
+
+
+btn.addEventListener("click", getSize  )
+
+function createContainer() {
+    if (!gridContainer) {
+         gridContainer = document.createElement("div");
+        gridContainer.id = "grid-container";
+        document.body.appendChild(gridContainer)
+        gridContainer.style.height = "100vh";
+        gridContainer.style.width = "960px";
+    } 
+}
+
+function removeContainer() {
+    if(gridContainer) {
+        gridContainer.remove();
+        gridContainer = null;
+    }
+
+}
+
+function drawGrid(size) {
+ 
+    const fragment1 = document.createDocumentFragment();
+    for (let i=1; i<=size; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList = `row-${i}`;
+        
+        fragment1.append(newDiv);
+    }
+    gridContainer.append(fragment1);
+    let rowDiv = document.querySelectorAll('[class^="row-"]');
+
+    for (let i=1; i<=size; i++) {
+        
+        let currentDiv = rowDiv[i-1];
+        let fragment2 = document.createDocumentFragment();
+        for (let j=1; j<=size; j++) {
+            let innerDiv = document.createElement("div");
+            innerDiv.className = `inner-${i}-${j}`;
+            fragment2.append(innerDiv);
+        }
+        currentDiv.append(fragment2);
+    }
 }
